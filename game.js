1d5bc9846f83473dd5df8d9c5add73ac25036fd3
Tiala Young <tialayoung@gmail.com>
2023-03-02 14:49:03 -0700

    Add setTimeout for delayed reset; commented out for now for testing purposes

--- game.js
+++ game.js
@@ -23,7 +23,7 @@ class Game {
     
     this.currentTurn = this.startingTurn;
     console.log(`New game started`)
-    console.log(JSON.parse(JSON.stringify(this)));
+    // console.log(JSON.parse(JSON.stringify(this)));
   };
 
   takeTurn(gridSpaceChosen) {
@@ -53,51 +53,60 @@ class Game {
 
   checkWinConditions() {
     console.log(`Checking...`)
-    // If a win statement is met: end this game, display winner & update player's win count, then call startNewGame
+    // If a win statement is met: end this game, display winner & update player's win count, then call startNewGame AFTER a 5 sec timeout
 
     if (this.gridSpaces[0] && this.gridSpaces[0] === this.gridSpaces[1] && this.gridSpaces[0] === this.gridSpaces[2]) {
       console.log(`WINNER if 1:`)
       this.players[this.gridSpaces[0]-1].increaseWins();
       console.log(this.players[this.gridSpaces[0]-1])
       this.startNewGame();
+      // setTimeout(this.startNewGame, 5000);
     } else if (this.gridSpaces[0] && this.gridSpaces[0] === this.gridSpaces[3] && this.gridSpaces[0] === this.gridSpaces[6]) {
       console.log(`WINNER if 2:`)
       this.players[this.gridSpaces[0]-1].increaseWins();
       console.log(this.players[this.gridSpaces[0]-1])
       this.startNewGame();
+      // setTimeout(this.startNewGame, 5000);
     } else if (this.gridSpaces[0] && this.gridSpaces[0] === this.gridSpaces[4] && this.gridSpaces[0] === this.gridSpaces[8]) {
       console.log(`WINNER if 3:`)
       this.players[this.gridSpaces[0]-1].increaseWins();
       console.log(this.players[this.gridSpaces[0]-1])
       this.startNewGame();
+      // setTimeout(this.startNewGame, 5000);
     } else if (this.gridSpaces[1] && this.gridSpaces[1] === this.gridSpaces[4] && this.gridSpaces[1] === this.gridSpaces[7]) {
       console.log(`WINNER if 4:`)
       this.players[this.gridSpaces[1]-1].increaseWins();
       console.log(this.players[this.gridSpaces[1]-1])
       this.startNewGame();
+      // setTimeout(this.startNewGame, 5000);
     } else if (this.gridSpaces[2] && this.gridSpaces[2] === this.gridSpaces[5] && this.gridSpaces[2] === this.gridSpaces[8]) {
       console.log(`WINNER if 5:`)
       this.players[this.gridSpaces[2]-1].increaseWins();
       console.log(this.players[this.gridSpaces[2]-1])
       this.startNewGame();
+      // setTimeout(this.startNewGame, 5000);
     } else if (this.gridSpaces[2] && this.gridSpaces[2] === this.gridSpaces[4] && this.gridSpaces[2] === this.gridSpaces[6]) {
       console.log(`WINNER if 6:`)
       this.players[this.gridSpaces[2]-1].increaseWins();
       console.log(this.players[this.gridSpaces[2]-1])
       this.startNewGame();
+      // setTimeout(this.startNewGame, 5000);
     } else if (this.gridSpaces[3] && this.gridSpaces[3] === this.gridSpaces[4] && this.gridSpaces[3] === this.gridSpaces[5]) {
       console.log(`WINNER if 7:`)
       this.players[this.gridSpaces[3]-1].increaseWins();
       console.log(this.players[this.gridSpaces[3]-1])
       this.startNewGame();
+      // setTimeout(this.startNewGame, 5000);
     } else if (this.gridSpaces[6] && this.gridSpaces[6] === this.gridSpaces[7] && this.gridSpaces[6] === this.gridSpaces[8]) {
       console.log(`WINNER if 8:`)
       this.players[this.gridSpaces[6]-1].increaseWins();
       console.log(this.players[this.gridSpaces[6]-1])
       this.startNewGame();
+      // setTimeout(this.startNewGame, 5000);
     } else if (this.gridSpaces[0] && this.gridSpaces[1] && this.gridSpaces[2] && this.gridSpaces[3] && this.gridSpaces[4] && this.gridSpaces[5] && this.gridSpaces[6] && this.gridSpaces[7] && this.gridSpaces[8]) {
       console.log(`DRAW! all spaces filled`)
       this.startNewGame();
+      // setTimeout(this.startNewGame, 5000);
     } else {
       console.log(`not a win & spaces still empty, game continues`)
       return;
