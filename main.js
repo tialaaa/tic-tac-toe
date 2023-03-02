f60897a90d7d75b4ab0eff8e7c2601d8424ec212
Tiala Young <tialayoung@gmail.com>
2023-03-02 13:56:19 -0700

    Revised test conditions for updated takeTurn function

--- main.js
+++ main.js
@@ -5,69 +5,53 @@ newGame.startNewGame();
 // console.log(JSON.parse(JSON.stringify(newGame)));
 
 newGame.takeTurn(4);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(2);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(8);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(5);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(1);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(7);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(6);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(0);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(3);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 // ^^^ ENDS IN A DRAW
 
 newGame.takeTurn(0);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(6);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(1);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(5);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(2);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 // ^^^ STARTING PLAYER WINS
 
 newGame.takeTurn(5);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
 
 newGame.takeTurn(2);
-newGame.changeTurnPlayer();
 newGame.checkWinConditions();
