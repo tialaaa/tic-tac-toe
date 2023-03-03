c7b5e9401e6c7b6b8be2cef4e9737f47e23a4fff
Tiala Young <tialayoung@gmail.com>
2023-03-02 20:01:04 -0700

    Add query selectors and begin event listener functions

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
+var gameGrid = document.querySelector('#game-container');
+var gameDisplay = document.querySelector('h1')
+var player1Display = document.querySelector('#player1');
+var player2Display = document.querySelector('#player2');
 var newGame = new Game();
 
 newGame.startNewGame();
+
+gameGrid.addEventListener('click', function() {
+  event.preventDefault();
+  identifyGridSpace
+});
+
+function identifyGridSpace() {
+  // var gridSpaceChosen = 
+  // use event.targetClosest? to identify which id was clicked
+  
+  newGame.takeTurn(gridSpaceChosen);
+  updateGameDisplay;
+  updatePlayerDisplay;
+  newGame.changeTurnPlayer();
+  newGame.checkWinConditions();
+};
+
+function updateGameDisplay() {
+  gameGrid.innerText()
+  // 
+};
+
+function updatePlayerDisplay() {
+  // when a player wins a game, use this.currentTurn
+  // match to correct playerXDisplay and update
+  // their display innerText to new this.players.wins count
+  if (newGame.currentTurn.id === 1) {
+    player1Display.innerText = ``;
+    
+  };
+};
+
+// ~~* CONSOLE TEST CASES BELOW FOR NOW *~~
 
 // console.log(JSON.parse(JSON.stringify(newGame)));
 
