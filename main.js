7d581b5111499366bcbc5148648848150bbad7c2
Tiala Young <tialayoung@gmail.com>
2023-03-07 11:11:13 -0700

    Refactor storePlayerScores and retrievePlayerScores using For loops

--- main.js
+++ main.js
@@ -5,34 +5,21 @@ var playerBanners = document.querySelectorAll('.player-banners');
 var animations = document.querySelectorAll('.animation');
 var clearButton = document.querySelector('#clear')
 
-// after a win, use window.localStorage.setItem(key, value) to store the current players' scores
-  // alternative:  window.localStorage.setItem('user', JSON.stringify(person));
-
-// event listener for page load, calls getItem to retrieve players' scores
-// then parses to turn back into numbers ?
-// then overwrites this.players[i].wins with those numbers
-// and call updatePlayerBanners
-
-// add a "clear history" button
-// using button, add Event listener for 'click' => will force clear player history
-// window.localStorage.clear();
-
 var newGame = new Game();
 
 newGame.refreshDataModel();
 clearGameGrid();
 
 window.addEventListener('load', function() {
-  // window.localStorage.clear()
   retrievePlayerScores();
 });
 
 clearButton.addEventListener('click', function() {
-  window.localStorage.clear()
+  window.localStorage.clear();
   retrievePlayerScores();
   updatePlayerBanners();
-  console.log(`Clear clicked`)
-})
+  console.log(`Clear clicked`);
+});
 
 gameGrid.addEventListener('click', function() {
   event.preventDefault();
@@ -43,56 +30,56 @@ gameGrid.addEventListener('click', function() {
 });
 
 function storePlayerScores() {
-  // var localStorageScores = [];
+  var localStorageScores = [];
 
-  // for (var i = 0; i < newGame.players.length; i++) {
-  //   if (newGame.players[i].wins === 0) {
-  //     localStorageScores.push(0);
-  //   } else {
-  //     localStorageScores.push(newGame.players[i].wins);
-  //   };
-  // };
-  // window.localStorage.setItem('playerScores', JSON.stringify(localStorageScores));
-  // console.log(localStorageScores)
+  for (var i = 0; i < newGame.players.length; i++) {
+    localStorageScores.push(newGame.players[i].wins);
+  };
+  window.localStorage.setItem('playerScores', JSON.stringify(localStorageScores));
+  console.log(`Stored locally as array:`);
+  console.log(localStorageScores)
 
-  // window.localStorage.setItem('player1Score', newGame.players[0].wins);
-  // window.localStorage.setItem('player2Score', newGame.players[1].wins);
-  window.localStorage.setItem('player1Score', JSON.stringify(newGame.players[0].wins));
-  window.localStorage.setItem('player2Score', JSON.stringify(newGame.players[1].wins));
-}
+  // window.localStorage.setItem('player1Score', JSON.stringify(newGame.players[0].wins));
+  // window.localStorage.setItem('player2Score', JSON.stringify(newGame.players[1].wins));
+};
 
 function retrievePlayerScores() {
-  var player1Score = JSON.parse(window.localStorage.getItem('player1Score'));
-  var player2Score = JSON.parse(window.localStorage.getItem('player2Score'));
-  console.log(`Local storage: ${player1Score}, ${player2Score}`)
+  var localStorageScores = JSON.parse(window.localStorage.getItem('playerScores'));
+  console.log(`Retrieved locally:`)
+  console.log(localStorageScores)
   console.log(`NewGame before updating players.win:`)
   console.log(JSON.parse(JSON.stringify(newGame)));
 
-  // newGame.players[0].wins = player1Score
-  // newGame.players[1].wins = player2Score
-  if (player1Score === null) {
-    newGame.players[0].wins = 0;
-  } else {
-    newGame.players[0].wins = player1Score;
-  }
+  for (var i = 0; i < localStorageScores.length; i++) {
+    if (localStorageScores[i] === null) {
+      newGame.players[i].wins = 0;
+    } else {
+      newGame.players[i].wins = localStorageScores[i];
+    }
+  };
 
-  if (player2Score === null) {
-    newGame.players[1].wins = 0;
-  } else {
-    newGame.players[1].wins = player2Score;
-  }
+  // var player1Score = JSON.parse(window.localStorage.getItem('player1Score'));
+  // var player2Score = JSON.parse(window.localStorage.getItem('player2Score'));
+  // console.log(`Local storage: ${player1Score}, ${player2Score}`)
+  // console.log(`NewGame before updating players.win:`)
+  // console.log(JSON.parse(JSON.stringify(newGame)));
+
+  // if (player1Score === null) {
+  //   newGame.players[0].wins = 0;
+  // } else {
+  //   newGame.players[0].wins = player1Score;
+  // }
+
+  // if (player2Score === null) {
+  //   newGame.players[1].wins = 0;
+  // } else {
+  //   newGame.players[1].wins = player2Score;
+  // }
 
   console.log(`NewGame after updating players.win:`)
   console.log(newGame)
   updatePlayerBanners();
-  // var localStorageScores = JSON.parse(window.localStorage.getItem('playerScores'));
-  // console.log(localStorageScores)
-  // console.log(newGame.players)
-
-  // for (var i = 0; i < localStorageScores.length; i++) {
-  //   newGame.players[i].wins = localStorageScores[i];
-  // };
-}
+};
 
 function findIndex() {
   for (var i = 0; i < gridSpaces.length; i++) {
